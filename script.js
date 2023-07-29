2dd6740e234dee0969510e4701dabb5880ebca37
Lammyy5 <reliford2010@gmail.com>
2023-07-29 08:58:07 -0700

    changed javascript code to add alert

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 // Assignment Code
-var numbers = ["0","1","2",'3','4','5','6','7','8','9'];
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowercase = ['a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var special = ['!','@','#','$','%','^','&','*','-','+','=','|','?','<','>','`','~','_'];
-var possible = [];
+ const numbers = '0123456789';
+ const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+ const lowercase = 'abcefghijklmnopqrstuvwxyz';
+ const special = '!@#$%^&*-+=|?<>`~_';
+
+ const characters = numbers + uppercase + lowercase + special;
+
 
 function generatePassword() {
   var howManycharacters = prompt("How many characters would you like your password to have? Between 8-128 characters.");
 howManycharacters = parseInt(howManycharacters,10)
  if (howManycharacters < 8 | howManycharacters > 128) {
   alert("Please enter a valid password between 8 - 128.");
+  return;
+ }
+ else if (isNaN(howManycharacters)) {
+  howManycharacters = alert("Please enter a valid number.")
+ } else { 
+  var uppercase = confirm("would you like your password to include uppercase?");
+if (uppercase) {
+  
+}
  }
 }
+
+
 
 var generateBtn = document.querySelector("#generate");
 
