9c58ef5600284c929bf5d8a0670e6b079248cfad
Lammyy5 <reliford2010@gmail.com>
2023-10-26 11:37:04 -0700

    Added + to each if statement which in turn fixed the random number generator from printing all of the characters at once, also fixed a mime issue with the css file that wasnt appyling the styles to the page

--- script.js
+++ script.js
@@ -1,67 +1,62 @@
 // Assignment Code
-const numbers = '0123456789';
-const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerCase = 'abcefghijklmnopqrstuvwxyz';
-const special = '!@#$%^&*-+=|?<>`~_';
-const characters = [numbers, upperCase, lowerCase, special];
+const numbers = '0123456789'
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerCase = 'abcefghijklmnopqrstuvwxyz'
+const special = '!@#$%^&*-+=|?<>`~_'
+const characters = [numbers, upperCase, lowerCase, special]
 
+function generatePassword() { 
+  let charChoice = window.prompt(
+    'How many characters would you like your password to have? Between 8-128 characters.'
+  )
+  
+  let charChoiceNum = parseInt(charChoice)
+  let passChars = []
 
-function generatePassword() {
-  var charChoice = window.prompt("How many characters would you like your password to have? Between 8-128 characters.");
-  var charChoiceNum = parseInt(charChoice, 10);
-  var passChars = [];
-
+   
 
 
   if (isNaN(charChoiceNum) || charChoiceNum < 8 || charChoiceNum > 128) {
-    window.alert("Please enter a valid number.");
-    generatePassword();
+    window.alert('Please enter a valid number.')
+    generatePassword()
   }
-  if (window.confirm("Do you want to use uppercase characters?")) {
-    passChars = passChars.concat(upperCase)
+  if (window.confirm('Do you want to use uppercase characters?')) {
+    passChars += passChars.concat(upperCase)
   }
-  if (window.confirm("Do you want to use lowercase characters?")) {
-    passChars = passChars.concat(lowerCase)
+  if (window.confirm('Do you want to use lowercase characters?')) {
+    passChars += passChars.concat(lowerCase)
   }
-  if (window.confirm("Do you want to use numbers characters?")) {
-    passChars = passChars.concat(numbers)
+  if (window.confirm('Do you want to use numbers characters?')) {
+    passChars += passChars.concat(numbers)
   }
-  if (window.confirm("Do you want to use special characters?")) {
-    passChars = passChars.concat(special)
+  if (window.confirm('Do you want to use special characters?')) {
+    passChars += passChars.concat(special)
   }
-  ;
-  if (passChars == false) {
-    window.alert("Must have at least one character choice selected");
-    generatePassword();
+  if (passChars === false) {
+    window.alert('Must have at least one character choice selected')
+    generatePassword()
   }
-  var password = "";
 
 
+  let password = ''
 
   for (let i = 0; i < charChoiceNum; i++) {
-    var randomIndex = (Math.floor(Math.random() * passChars.length));
-    password = passChars[randomIndex];
+    const randomIndex = Math.floor(Math.random() * passChars.length)
+    password += passChars[randomIndex]
+    console.log(password)
   }
-  return password;
+  return password
 }
 
-
-
-
-
-
-
-
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword()
+  var passwordText = document.querySelector('#password')
 
-  passwordText.value = password;
-
+  passwordText.value = password
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword)
